4d5ce0c8c233c75f6d6d45c3eee116ebaa653651
naomichi-y <n.yamakita@gmail.com>
2015-07-04 02:05:11 +0900

    fixed #16

--- source/app/tests/controllers/DashboardControllerTest.php
+++
@@ -1,7 +0,0 @@
-<?php
-class DashboardControllerTest extends TestCase {
-  public function testIndex()
-  {
-    $this->assertUserOnlyContent('GET', '/dashboard');
-  }
-}
--- source/app/tests/controllers/IndexControllerTest.php
+++
@@ -1,7 +0,0 @@
-<?php
-class IndexControllerTest extends TestCase {
-  public function testIndex()
-  {
-    $this->assertGuestOnlyContent('GET', '/');
-  }
-}
