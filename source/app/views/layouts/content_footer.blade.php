e6d790eb4b64ad3e96f2ee8ec0b7f3c6219c9982
naomichi-y <n.yamakita@gmail.com>
2016-09-24 17:04:13 +0900

    Update header/footer

--- source/app/views/layouts/content_footer.blade.php
+++ source/app/views/layouts/content_footer.blade.php
@@ -1,7 +1,24 @@
 <footer>
   <ul class="list-unstyled">
     <li>{{link_to('contact', 'お問い合わせ')}}</li>
+<!--
     <li>{{link_to('http://about.me/naomichi.yamakita', '運営')}}</li>
     <li>{{link_to('https://github.com/naomichi-y/monelytics', 'GitHub')}}</li>
+  -->
   </ul>
 </footer>
+
+<!--
+<script type="text/javascript">(function(){var t,e,r,n,c;for(t=function(){var t;return t=t||[],function(){var e,r,n,c;for(r=["init","start","stop","user","track","action","event","goal","chat","buy","page","view","admin","group","alias","ready","link","form","click","submit","cmd","emit","on","send","css","js","style","option","get","set"],e=function(e){return function(){return t.push([e].concat(Array.prototype.slice.call(arguments,0)))}},n=0,c=[];n<r.length;)t[r[n]]=e(r[n]),c.push(n++);return c}(),t.init=function(e,r){var n,c;return t.api_key=e,t.options=r||{},c=document.createElement("script"),c.type="text/javascript",c.async=!0,c.charset="utf-8",c.src="https://static.karte.io/libs/tracker.js",n=document.getElementsByTagName("script")[0],n.parentNode.insertBefore(c,n)},t},c=window.karte_tracker_names||["tracker"],r=0,n=c.length;n>r;r++)e=c[r],window[e]=t();tracker.init("1c221fad4b4c4a57f60b7c03c0e34152")}).call(this);</script>
+-->
+
+<script src="//automation-metaps.akamaized.net/tag/master/metaps_automation.js"></script>
+<script>
+  var config = {
+    'mode': 'live',
+    'debug': true,
+    'engagement': false
+  };
+  var metapsAutomation = new MetapsAutomation('ak_Dkm3eaf', config);
+  metapsAutomation.tracking();
+</script>
