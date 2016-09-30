6451897a0c4fc3df7651e1a6bd417acdbbec6173
naomichi-y <n.yamakita@gmail.com>
2016-09-30 22:39:03 +0900

    Update phpunit settings

--- docker/db/docker-entrypoint-initdb.d/00-create_user.sql
+++ docker/db/docker-entrypoint-initdb.d/00-create_user.sql
@@ -1,3 +1,5 @@
 CREATE USER 'webapp'@'%' IDENTIFIED BY 'jiD7mkaS72';
 CREATE DATABASE monelytics DEFAULT CHARACTER SET utf8mb4;
+CREATE DATABASE monelytics_testing DEFAULT CHARACTER SET utf8mb4;
 GRANT ALL ON monelytics.* TO 'webapp'@'%';
+GRANT ALL ON monelytics_testing.* TO 'webapp'@'%';
