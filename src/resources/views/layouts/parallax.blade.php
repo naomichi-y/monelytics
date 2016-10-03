bb9674c729a2df0f8af47948cf2d7fa0f848b044
naomichi-y <n.yamakita@gmail.com>
2016-10-03 23:54:57 +0900

    Fix parallax JS error

--- src/resources/views/layouts/parallax.blade.php
+++ src/resources/views/layouts/parallax.blade.php
@@ -2,7 +2,7 @@
 <html>
     @section('extend_head_tags')
         {!! Html::style('assets/css/parallax.css') !!}
-        {!! Html::script('assets/components/jquery_plugins/parallax.js-1.3.1/parallax.min.js') !!}
+        {!! Html::script('assets/components/jquery_plugins/parallax.js/1.4.2/parallax.min.js') !!}
     @stop
     @include('layouts.head_tags')
     @yield('include_header')
