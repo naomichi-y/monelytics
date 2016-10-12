5865a9f84d32b72d5784f767f3fdb4eadf058c8a
naomichi-y <n.yamakita@gmail.com>
2016-10-12 23:17:57 +0900

    Change directory structure

---
+++ vendor/oriceon/oauth-5-laravel/src/Artdarek/OAuth/Facade/OAuth.php
@@ -0,0 +1,19 @@
+<?php namespace Artdarek\OAuth\Facade;
+
+/**
+ * @author     Dariusz Prząda <artdarek@gmail.com>
+ * @copyright  Copyright (c) 2013
+ * @license    http://www.opensource.org/licenses/mit-license.html MIT License
+ */
+
+use Illuminate\Support\Facades\Facade;
+
+class OAuth extends Facade {
+
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor() { return 'oauth'; }
+}
---
+++ vendor/oriceon/oauth-5-laravel/src/Artdarek/OAuth/OAuthServiceProvider.php
@@ -0,0 +1,60 @@
+<?php namespace Artdarek\OAuth;
+
+/**
+ * @author     Dariusz Prząda <artdarek@gmail.com>
+ * @copyright  Copyright (c) 2013
+ * @license    http://www.opensource.org/licenses/mit-license.html MIT License
+ */
+
+use Illuminate\Support\ServiceProvider;
+
+class OAuthServiceProvider extends ServiceProvider {
+
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/../../config/config.php' => config_path('oauth-5-laravel.php'),
+        ], 'config');
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // Register 'oauth'
+        $this->app['oauth'] = $this->app->share(function ($app)
+        {
+            // create oAuth instance
+            $oauth = new OAuth();
+
+            // return oAuth instance
+            return $oauth;
+        });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [];
+    }
+
+}
